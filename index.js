b170b914f57811737928115ed3620310a8ff21bd
Seth Barlow <sethdbarlow@gmail.com>
2022-12-09 14:36:11 -0700

    commit tree 6394faf2708112db961b781ace4b39144020faf3

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name: "seth",
+    streetAddress: "14161 Deer Haven cv",
+}
+
+function updateEmployeeWithKeyAndValue(employeeObject, Key, Value) {
+    const newObj = {...employee};
+    newObj[Key] = Value;
+    return newObj;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, Key, Value) {
+    obj[Key] = Value;
+    return obj;
+}
+
+
+function deleteFromEmployeeByKey(obj, Key) {
+ const newObj = {...employee};
+ delete newObj[Key];
+ return newObj
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(obj, Key) {
+ delete employee[Key]
+ return employee;
+}
+
