60a7d93be521305496c922eec2c9daf1865e288a
Seth Barlow <sethdbarlow@gmail.com>
2023-02-01 10:29:56 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "seth",
+    streetAddress: "280 Iowa Rd",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+
+    newObj[key] = value;
+  
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = {...employee};
+    delete newObj[key]
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
